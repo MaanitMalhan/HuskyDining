79238ef87b1a16a5eb0e2014cc5096445ea64d13
Maanit972 <maanitmalhan@gmail.com>
2024-10-28 13:46:40 -0400

    menu page almost done

--- frontend/src/page/Menu.js
+++ frontend/src/page/Menu.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Header from '../components/header';
-import Footer from '../components/footer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Menu = () => {
     return (
-        <div>
-            <Header />
-            <div className='menuHeading'>
-                <h1> Dining Hall Menus </h1>
+    <div className='menu-container'>
+    <Header />
+        <div className='menuHeading'>
+        <h1> Storrs Dining Hall Menus </h1>
+
+                <div className='hallListsWrapper'>
             </div> 
 
             <div className='hallList'>
@@ -60,9 +62,84 @@ const Menu = () => {
                 </a>            
                 </ol>
             </div>
-            <h1>Resturants Around Campus</h1>
-            <Footer />
+           
+            <div className='menuHeading'>
+                <h1>Resturants Around Storrs Campus</h1>
+            </div>
+           
+            <div className='hallList'>
+            <ol>
+                <a href="https://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons,%20Halal+%26+Kosher&naFlag=1" target="_blank" rel="noopener noreferrer">
+                    Earth Wok and Fire
+                </a>            
+            </ol>
+
+            <ol>
+                <a href="https://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons,%20Halal+%26+Kosher&naFlag=1" target="_blank" rel="noopener noreferrer">
+                    Food Truck
+                </a>            
+            </ol>
+
+            <ol>
+                <a href="https://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons,%20Halal+%26+Kosher&naFlag=1" target="_blank" rel="noopener noreferrer">
+                    Mango
+                </a>            
+            </ol>
+
+            <ol>
+                <a href="https://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons,%20Halal+%26+Kosher&naFlag=1" target="_blank" rel="noopener noreferrer">
+                    One Plate, Two Plates
+                </a>            
+            </ol>
+
+            <ol>
+                <a href="https://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons,%20Halal+%26+Kosher&naFlag=1" target="_blank" rel="noopener noreferrer">
+                    UC-Cafes - Bookworms Cafe
+                </a>            
+            </ol>
+
+            <ol>
+                <a href="https://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons,%20Halal+%26+Kosher&naFlag=1" target="_blank" rel="noopener noreferrer">
+                    UC-Cafes The Beanery
+                </a>            
+            </ol>
+
+            <ol>
+                <a href="https://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons,%20Halal+%26+Kosher&naFlag=1" target="_blank" rel="noopener noreferrer">
+                    UC-Cafes Nanobyte
+                </a>            
+            </ol>
+            
+            <ol>
+                <a href="https://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons,%20Halal+%26+Kosher&naFlag=1" target="_blank" rel="noopener noreferrer">
+                    UC-Cafes Crossroads
+                </a>            
+            </ol>
+
+            <ol>
+                <a href="https://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons,%20Halal+%26+Kosher&naFlag=1" target="_blank" rel="noopener noreferrer">
+                    UC-Cafes Market Cafe
+                </a>            
+            </ol>
+
+            <ol>
+                <a href="https://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons,%20Halal+%26+Kosher&naFlag=1" target="_blank" rel="noopener noreferrer">
+                    Dairy Bar
+                </a>            
+            </ol>
+
+            <ol>
+                <a href="https://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=42&locationName=Gelfenbien+Commons,%20Halal+%26+Kosher&naFlag=1" target="_blank" rel="noopener noreferrer">
+                    Union Street Market
+                </a>            
+            </ol>
+            </div>
+            
         </div>
+     <Footer />
+
+    </div>
+        
     );
 };
 
