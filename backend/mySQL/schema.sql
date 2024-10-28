a247c031e4d702901496aed7b5ff449af9f31d29
Alexander Hamilton <alexander0hamilton@gmail.com>
2024-10-28 12:43:39 -0400

    some stuff

--- backend/mySQL/schema.sql
+++ backend/mySQL/schema.sql
@@ -1,15 +1,18 @@
 CREATE DATABASE dining_hall;
 USE dining_hall;
 
-CREATE TABLE students(
-    peoplesoft integer PRIMARY KEY AUTO_INCREMENT,
-    netid VARCHAR(255),
+CREATE TABLE students (
+    peoplesoft INT NOT NULL AUTO_INCREMENT,
+    netid VARCHAR(255) NOT NULL,
+    password VARCHAR(255) DEFAULT 'password',
     name VARCHAR(255) NOT NULL,
-    points integer,
-    flex_passes integer
+    points INT,
+    flex_passes INT,
+    PRIMARY KEY (peoplesoft)
 );
+
 
 -- INSERT INTO students (netid, name, points, flex_passes)
 -- VALUES
--- ('vfsdn1234', 'John Doe', 200, 40),
--- ('krvj1234', 'Jane Doe', 200, 40);
+-- ('fha60182', 'John Doe', 200, 40),
+-- ('vjah2145', 'Jane Doe', 200, 40);
