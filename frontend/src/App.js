f0d8be380bec7b6405aa4f9b5ea1df039e2d926b
Alexander Hamilton <alexander0hamilton@gmail.com>
2024-11-04 13:13:45 -0500

    added request page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './page/home';  // Import Home component
 import Menu from './page/Menu';
+import Request from './page/request'
 import Menusupport from './components/Menusupport';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />   {/* Home Page */}
           <Route path="/menu" element={<Menu />} /> {/* Menu Page */}
+          <Route path="/request" element={<Request />} /> {/* Request Page */}
         </Routes>
       </Router>
     </div>
