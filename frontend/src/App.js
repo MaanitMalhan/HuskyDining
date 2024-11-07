1db1d188e3bc1a6d903641863ffc6e334e42f336
Maanit972 <maanitmalhan@gmail.com>
2024-11-07 08:33:01 -0500

    fixed issues post merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+
 import './App.css';
 import Home from './page/home';
 import Menu from './page/Menu';
@@ -10,7 +11,6 @@ import SignUp from './page/SignUp'; // Import SignUp component
 import Dashboard from './page/Dashboard'; // Import Dashboard component
 import PrivateRoute from './components/PrivateRoute'; // Import PrivateRoute for protecting routes
 import { AuthProvider } from './context/AuthContext'; // Import AuthProvider
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -20,26 +20,10 @@ function App() {
           <Route path="/" element={<Home />} />   {/* Home Page */}
           <Route path="/menu" element={<Menu />} /> {/* Menu Page */}
           <Route path="/request" element={<Request />} /> {/* Request Page */}
+          <Route path="/donation" element={<Donation />} /> {/* Donation Page */}
         </Routes>
       </Router>
 
-      <AuthProvider> {/* Wrap the app in AuthProvider to manage auth state */}
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />  {/* Home Page */}
-            <Route path="/menu" element={<Menu />} />  {/* Menu Page */}
-            <Route path="/login" element={<Login />} /> {/* Login Page */}
-            <Route path="/signup" element={<SignUp />} /> {/* Sign-Up Page */}
-            
-            {/* Protected Dashboard Route */}
-            <Route path="/dashboard" element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            } />
-          </Routes>
-        </Router>
-      </AuthProvider>
     </div>
   );
 }
