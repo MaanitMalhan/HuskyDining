bbaba8994a65b62064bb6f7c6360d155ff3dc2c0
Thudles <133913487+Thudles@users.noreply.github.com>
2024-12-03 11:38:55 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
-import Menu from './page/Menu';
-import Menusupport from './components/Menusupport';
+import "./App.css";
+import Home from "./page/home";
+import Menu from "./page/Menu";
+import Reviews from "./page/reviews";
+import Request from "./page/request";
+import Donation from "./page/Donation";
+import Menusupport from "./components/Menusupport";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./page/Login"; // Import Login component
+import SignUp from "./page/SignUp"; // Import SignUp component
+import Dashboard from "./page/Dashboard"; // Import Dashboard component
+import PrivateRoute from "./components/PrivateRoute"; // Import PrivateRoute for protecting routes
+import { AuthProvider } from "./context/AuthContext"; // Import AuthProvider
 
 function App() {
   return (
     <div className="App">
-      <Menu />
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} /> {/* Home Page */}
+            <Route path="/menu" element={<Menu />} /> {/* Menu Page */}
+            <Route path="/login" element={<Login />} /> {/* Login Page */}
+            <Route path="/signup" element={<SignUp />} /> {/* Sign-Up Page */}
+            <Route path="/reviews" element={<Reviews />} /> {/* Sign-Up Page */}
+          </Routes>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
