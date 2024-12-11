0442d73d6f32dca555caf38b52fb3231f1edefaf
Chikwendu Mgbechi <chikwe2002@gmail.com>
2024-12-11 12:29:16 -0500

    fixed faulty import

--- frontend/src/pages/Reviews.jsx
+++ frontend/src/pages/Reviews.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Review from "../components/Review/Reviews";
-import Review_test from "../components/Review/Review_test";
 
 const Reviews = () => {
   return (
