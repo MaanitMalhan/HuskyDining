1337fab5a7a92f66f2611094485fe9a9a3c230aa
Alexander Hamilton <alexander0hamilton@gmail.com>
2025-04-17 20:48:53 -0400

    fixed donate

--- Backend/controllers/transactionController.js
+++ Backend/controllers/transactionController.js
@@ -155,23 +155,43 @@ const requestPointsTransaction = asyncHandler(async (req, res) => {
 // route GET /api/transaction/donate
 // @access Private
 const donateTransaction = asyncHandler(async (req, res) => {
-  const { fromUserId, toUserId, amount } = req.body;
+  const { fromEmail, toEmail, amount, type } = req.body;
 
-  if (!fromUserId || !toUserId || !amount) {
+
+  if (!fromEmail || !toEmail || !amount || !type) {
     res.status(400);
     throw new Error(
-      "Please provide all required fields: fromUserId, toUserId, amount"
+      "Please provide all required fields"
     );
   }
+
+  const fromUser = await User.findOne({ email: fromEmail });
+  const toUser = await User.findOne({ email: toEmail });
+
+  console.log(req.body);
+
+  if (!fromUser || !toUser) {
+    res.status(404);
+    throw new Error("One or both users not found");
+  }
+
+  const fromUserId = fromUser._id;
+  const toUserId = toUser._id;
+
+  console.log(fromUserId);
+  console.log(toUserId);
 
   const session = await mongoose.startSession();
   try {
     session.startTransaction();
 
+    const balanceField =  type === "flexpass" ? "flexpass" :
+                          type === "points" ? "points" : "balance";
+
     // Subtract donation amount from sender
     const sender = await User.findByIdAndUpdate(
       fromUserId,
-      { $inc: { balance: -amount } },
+      { $inc: { [balanceField]: -amount } },
       { new: true, session }
     );
 
@@ -186,7 +206,7 @@ const donateTransaction = asyncHandler(async (req, res) => {
     // Add donation amount to recipient
     const recipient = await User.findByIdAndUpdate(
       toUserId,
-      { $inc: { balance: amount } },
+      { $inc: { [balanceField]: amount } },
       { new: true, session }
     );
 
@@ -199,9 +219,10 @@ const donateTransaction = asyncHandler(async (req, res) => {
     await Ledger.create({
       userID: fromUserId,
       recipientID: toUserId,
-      recipient_name: recipientUser.name,
-      amount: amount,
-      type: "Donate",
+      recipient_name: recipient.name,
+      amount,
+      type: type === "flexpass" ? "Flex" :
+            type === "points" ? "Points" : "Donate",
     });
 
     // Commit the transaction if both updates succeed
